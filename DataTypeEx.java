c39835e1f31a9a7abde8625ceb78c2a67b9e0b37
kavyareddy1118 <yegamatikavya.reddy@gmail.com>
2025-06-03 12:13:33 +0530

    Add files via upload

---
+++ DataTypeEx.java
@@ -0,0 +1,18 @@
+public class DataTypeEx
+{
+  public static void main(String arga[])
+{
+    char ch='a';
+    Boolean value=true;
+    byte num1=100;
+    short num2=200;
+    int num3=1200;
+    long num4=120000;
+    float num5=3.14f;
+    double num6=321.09876;
+   System.out.println(ch+","+ value);
+    System.out.println("Integer Type:"+num1+","+num2+","+num3+","+num4);
+    System.out.println("Floating values:"+num5+","+num6);
+}
+}
+   
