a78c844173646f7bdb9b4fb99d52720572f59be2
kavyareddy1118 <yegamatikavya.reddy@gmail.com>
2025-06-04 23:09:06 +0530

    Add files via upload

---
+++ BitwiseOp.java
@@ -0,0 +1,17 @@
+public class BitwiseOp
+{
+   public static void main(String args[])
+{
+   int num1=10;
+   int num2=12;
+   System.out.println(num1&num2);
+   System.out.println(num1|num2);
+   System.out.println(num1^num2);
+}
+}
+
+
+
+
+
+  
