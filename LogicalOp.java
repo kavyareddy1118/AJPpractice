446a18e61bc85dadffa5e4d403a74ce3a3aca40d
kavyareddy1118 <yegamatikavya.reddy@gmail.com>
2025-06-04 22:57:49 +0530

    Add files via upload

---
+++ LogicalOp.java
@@ -0,0 +1,12 @@
+public class LogicalOp
+{
+public static void main(String args[])
+{
+  boolean val1=true;
+  Boolean val2=false;
+  System.out.println(val1&&val2);
+   int num1=12;
+   int num2=10;
+   System.out.println((num1>num2)||(num1<num2));
+}
+}
