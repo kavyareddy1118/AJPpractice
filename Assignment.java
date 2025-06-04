6f1c122c64e131c8fe8fe864023dc676fdf7c8e4
kavyareddy1118 <yegamatikavya.reddy@gmail.com>
2025-06-04 22:28:19 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+class Assignment
+{
+    public static void main(String[] args)
+    {
+    //Declaring variables
+        int num;
+        String name;
+
+        // Assigning values
+        num = 18;
+        name = "smitha";
+
+        // Displaying the assigned values
+        System.out.println("num is assigned: " + num);
+        System.out.println("name is assigned: " + name);
+    }
+}
